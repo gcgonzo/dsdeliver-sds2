ae18355c5970e6a86e78413edf8e2297e423c0ab
Gilnei <gilbegprog@gmail.com>
2021-01-08 11:01:26 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,18 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import Routes from './Routes';
 
 
 function App() {
   return (
+    <>
+      <Routes />
+      <ToastContainer />
 
-     <Routes />
-   
+    </>
+      
   );
 }
 
